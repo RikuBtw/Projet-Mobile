e94c29f6c9a3642a9eb54b6000adc8e9ecad9a64
Julien AUBERT-- BEDUCHAUD <E154817E@U-INF-J-F222-19.iut-nantes.univ-nantes.prive>
2017-03-22 14:16:20 +0100

    commit 1

---
+++ Appli_cinéma/app/src/androidTest/java/com/example/e154817e/appli_cinma/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.example.e154817e.appli_cinma;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
